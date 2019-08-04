88c07edf4bfe66fbbe62f653f3d1ce0a4c72d323
faridho <faridho45@gmail.com>
2019-08-05 02:57:31 +0700

    update

---
+++ src/views/LPJ/FormManage/List/index.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <v-list two-line subheader>
+      <v-subheader inset></v-subheader>
+      <v-list-tile
+        v-for="item in items" :key="item.id"
+        rel="noopener nofollow"
+        ripple
+        @click="detail(item.id, $event.target)"
+        avatar>
+        <v-list-tile-avatar>
+          <v-icon>mdi-file-restore mdi-48px </v-icon>
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.tgl_keluar }}</v-list-tile-title>
+        </v-list-tile-content>
+        <v-list-tile-action>
+          <v-icon color="warning">mdi-eye-check</v-icon>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
+    <v-dialog
+      v-model="dialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition">
+      <v-card>
+        <v-card-title>
+          <h3>Detail Pengeluaran</h3>
+        </v-card-title>
+        <v-card-text>
+          <v-list two-line>
+
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon color="indigo">mdi-file</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-sub-title>Jumlah Pengeluaran</v-list-tile-sub-title>
+                {{ jumlahPengeluaran }}
+              </v-list-tile-content>
+            </v-list-tile>
+
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon color="indigo">mdi-locker-multiple</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-sub-title>Keterangan</v-list-tile-sub-title>
+                {{ keterangan }}
+              </v-list-tile-content>
+            </v-list-tile>
+
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon color="indigo">mdi-calendar</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-sub-title>Tgl Keluar</v-list-tile-sub-title>
+                {{ tglKeluar }}
+              </v-list-tile-content>
+            </v-list-tile>
+
+          </v-list>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn block flat color="warning" outline @click="close">Tutup</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script>
+import months from '@/views/list'
+import eventBus from '@/views/eventBus'
+import URL from '@/api/url'
+import axios from 'axios'
+
+export default {
+  data: () => ({
+    dialog: false,
+    items: [
+      {
+        "id": 1,
+        "jumlah_pengeluaran": 200000,
+        "keterangan": "PerbaikanPortal",
+        "tgl_keluar": "2019-08-04"
+      }
+    ],
+
+    jumlahPengeluaran: '',
+    keterangan: '',
+    tglKeluar: ''
+
+  }),
+
+  mounted() {
+    //this.getData();
+    eventBus.$on('DATA_LOADED', this.getData);
+  },
+
+  methods: {
+
+    async getData() {
+      const result = await axios
+        .get(URL + '/anouncement/get')
+        .then(response => response.data.data);
+
+      this.items = result;
+    },
+
+
+    async detail(target) {
+      this.dialog = true;
+
+      /*const detail = await axios
+        .get(URL + '/anouncement/get/' + target)
+        .then(response => response.data.data);*/
+
+      this.jumlahPengeluaran = 200000;
+      this.keterangan = "Perbaikan Portal";
+      this.tglKeluar = "2019-09-04"
+    },
+
+    close() {
+      this.dialog = false;
+    }
+  }
+}
+</script>
---
+++ src/views/LPJ/FormManage/index.vue
@@ -0,0 +1,130 @@
+<template>
+  <v-layout>
+    <v-flex xs12>
+      <v-card>
+        <v-navigation-drawer
+          fixed
+          v-model="drawer"
+          app>
+          <v-list
+            dense>
+            <v-list-tile
+              v-for="menu in menus" :key="menu.title"
+              :to="menu.href"
+              rel="noopener nofollow"
+              ripple>
+              <v-list-tile-action>
+                <v-icon>{{ menu.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ menu.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-navigation-drawer>
+        <v-toolbar color="warning" light extended>
+          <v-toolbar-side-icon class="white--text" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+          <v-toolbar-title class="white--text">LPJ Dana</v-toolbar-title>
+        </v-toolbar>
+        <MasterList />
+        <v-dialog
+          v-model="dialog"
+          fullscreen
+          hide-overlay
+          transition="dialog-bottom-transition">
+          <v-card>
+            <v-card-title>
+              <h3>Buat Pengeluaran</h3>
+            </v-card-title>
+            <v-card-text>
+              <v-form v-model="valid" ref="form" lazy-validation>
+                
+                <v-text-field
+                  v-model="jumlahPengeluaran"
+                  :rules="nameRules"
+                  label="Jumlah Pengeluaran"
+                >
+                </v-text-field>
+
+                <v-text-field
+                  v-model="keterangan"
+                  :rules="nameRules"
+                  label="Keterangan"
+                >
+                </v-text-field>
+
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn block flat color="warning" outline @click="close">Batal</v-btn>
+              <v-btn :loading="loading" block color="warning" @click="submit">Simpan</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+import eventBus from '@/views/eventBus'
+import URL from '@/api/url'
+import menus from '@/api/menu'
+import axios from 'axios'
+import MasterList from './List'
+
+export default {
+  data: () => ({
+    valid: true,
+    drawer: null,
+    dialog: false,
+    loading: false,
+
+    menus: menus,
+    nameRules: [v => !!v || "Kolom wajib diisi"],
+    
+    jumlahPengeluaran: '',
+    keterangan: ''
+  }),
+
+  methods: {
+
+    close() {
+      this.dialog = false;
+    },
+
+    async submit() {
+      this.loading = true;
+      const payload = {
+        jumlahPengeluaran: this.jumlahPengeluaran,
+        keterangan: this.keterangan
+      }
+
+
+      const storing = await axios
+        .post(URL + '/anouncement/insert', payload, undefined)
+        .then(response => response.data);
+
+      if (storing.status === true) {
+        this.dialog = false;
+        this.loading = false;
+        eventBus.$emit('DATA_LOADED');
+        this.$snackbar(storing.message);
+      } else {
+        this.loading = false;
+        this.$snackbar(storing.message);
+      }
+    }
+  },
+
+  components: {
+    MasterList
+  }
+}
+</script>
+<style>
+.margin-right {
+  margin-right: 0px;
+}
+</style>
+
+
