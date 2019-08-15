0d349b379af0a5b74ce50634afe04e4222404754
faridho <faridho@zeepos.com>
2019-08-16 01:36:07 +0700

    merge

--- src/api/url.js
+++ src/api/url.js
@@ -1,2 +1,3 @@
-const URL = 'http://apicollection.faridho.com/public/api/v1';
+//const URL = 'http://apicollection.faridho.com/public/api/v1';
+const URL = 'http://127.0.0.1:8000/api/v1';
 export default URL;
